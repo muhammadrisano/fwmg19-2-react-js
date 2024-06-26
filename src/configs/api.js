98c7959ab90ea5b012c36476e8bf181977aca41a
muhammad risano <muhammadrisano@gmail.com>
2024-06-26 13:32:40 +0700

    add refresh token

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,19 +1,56 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-})
-
-api.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  const token = localStorage.getItem('token')
-  if(token){
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-export default api
+api.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  async function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const originalRequest = error.config;
+
+    const message = error.response?.data?.message;
+    if (message === "token expired" && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem('resfreshToken')
+        const {data} =  await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh-token`, {
+          refreshToken: refreshToken
+        })
+        console.log(data);
+        localStorage.setItem('token', data.data.token)
+        localStorage.setItem('resfreshToken', data.data.refreshToken)
+        originalRequest.headers.Authorization = `Bearer ${data.data.token}`;
+
+        return api(originalRequest)
+      } catch (error) {
+        console.log('redirect ke login');
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
--- src/configs/redux/store.js
+++ src/configs/redux/store.js
@@ -1,8 +1,10 @@
-import {legacy_createStore as createStore, applyMiddleware} from 'redux'
+import {legacy_createStore as createStore, applyMiddleware, compose} from 'redux'
 import { thunk } from 'redux-thunk'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import rootReducer from './reducer/reducer'
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
